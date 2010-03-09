cec16f806da6506a73885ee8396f62f681dc0a77
Christian Jung <campino2k@gmail.com>
2010-03-09 14:11:50 +0100

    added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * German Language File
+ * @author Christian Jung <campino2k@gmail.com>
+ */
+
+$lang['current']    = 'Aktueller';
+$lang['go']         = 'Los';
+
+$lang['year']       = 'Jahr';
+$lang['mon']        = 'Monat';
+
+$lang['days'][1]    = 'Montag';
+$lang['days'][2]    = 'Dienstag';
+$lang['days'][3]    = 'Mittwoch';
+$lang['days'][4]    = 'Donnerstag';
+$lang['days'][5]    = 'Freitag';
+$lang['days'][6]    = 'Samstag';
+$lang['days'][7]    = 'Sonntag';
+
+$lang['month'][1]   = 'Januar';
+$lang['month'][2]   = 'Februar';
+$lang['month'][3]   = 'März';
+$lang['month'][4]   = 'April';
+$lang['month'][5]   = 'Mai';
+$lang['month'][6]   = 'Juni';
+$lang['month'][7]   = 'Juli';
+$lang['month'][8]   = 'August';
+$lang['month'][9]   = 'September';
+$lang['month'][10]  = 'Oktober';
+$lang['month'][11]  = 'November';
+$lang['month'][12]  = 'Dezember';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
