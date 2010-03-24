1da2ce6d7e38aecbbd7e5fd23f22cbde5a58d2dd
@author Florian Gaub <floriang@floriang.net>
2010-03-24 10:06:53 +0100

    added french translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * French Language File
+ * @author Florian Gaub <floriang@floriang.net>
+ */
+ 
+$lang['current']    = 'Mois actuel';
+$lang['go']         = 'Voir';
+
+$lang['year']       = 'Année ';
+$lang['mon']        = 'Mois ';
+ 
+$lang['days'][1]    = 'Lundi';
+$lang['days'][2]    = 'Mardi';
+$lang['days'][3]    = 'Mercredi';
+$lang['days'][4]    = 'Jeudi';
+$lang['days'][5]    = 'Vendredi';
+$lang['days'][6]    = 'Samedi';
+$lang['days'][7]    = 'Dimanche';
+ 
+$lang['month'][1]   = 'Janvier';
+$lang['month'][2]   = 'Février';
+$lang['month'][3]   = 'Mars';
+$lang['month'][4]   = 'Avril';
+$lang['month'][5]   = 'Mai';
+$lang['month'][6]   = 'Juin';
+$lang['month'][7]   = 'Juillet';
+$lang['month'][8]   = 'Août';
+//~ $lang['month'][8]   = 'Aout';
+$lang['month'][9]   = 'Septembre';
+$lang['month'][10]  = 'Octobre';
+$lang['month'][11]  = 'Novembre';
+$lang['month'][12]  = 'Décembre';
---
+++ lang/fr/settings.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * French settings file for DokuWiki Plugin wikicalendar
+ * @author Florian Gaub <floriang@floriang.net>
+ */
+$lang['weekstart'] = 'Jour du début de la semaine';
