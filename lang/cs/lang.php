f74b2ef636187354258e22d0464066cf1461e859
Vojtech Rylko <vojta.rylko@seznam.cz>
2010-06-15 04:12:36 +0800

    added czech translation

---
+++ lang/cs/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Czech Language File
+ * @author Vojtěch Rylko <vojta.rylko@seznam.cz>
+ */
+ 
+$lang['current']    = 'Dnes';
+$lang['go']         = 'Jdi';
+
+$lang['year']       = 'Rok';
+$lang['mon']        = 'Měsíc';
+ 
+$lang['days'][1]    = 'Pondělí';
+$lang['days'][2]    = 'Úterý';
+$lang['days'][3]    = 'Středa';
+$lang['days'][4]    = 'Čtvrtek';
+$lang['days'][5]    = 'Pátek';
+$lang['days'][6]    = 'Sobota';
+$lang['days'][7]    = 'Neděle';
+ 
+$lang['month'][1]   = 'Leden';
+$lang['month'][2]   = 'Únor';
+$lang['month'][3]   = 'Březen';
+$lang['month'][4]   = 'Duben';
+$lang['month'][5]   = 'Květen';
+$lang['month'][6]   = 'Červen';
+$lang['month'][7]   = 'Červenec';
+$lang['month'][8]   = 'Srpen';
+$lang['month'][9]   = 'Září';
+$lang['month'][10]  = 'Říjen';
+$lang['month'][11]  = 'Listopad';
+$lang['month'][12]  = 'Prosinec';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ lang/cs/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Czech settings file for DokuWiki Plugin wikicalendar
+ * @author Vojtěch Rylko <vojta.rylko@seznam.cz>
+ */
+$lang['weekstart'] = 'Týden začíná dnem';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
