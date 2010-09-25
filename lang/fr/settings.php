7f4b51de192a9de460d2df5c0028e7dcd2b8ec86
Floriang <floriang@floriang.net>
2010-09-25 12:17:21 +0200

    updated french translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,3 +4,4 @@
  * @author Florian Gaub <floriang@floriang.net>
  */
 $lang['weekstart'] = 'Jour du début de la semaine';
+$lang['timezone']  = 'Fuseau horaire (celui du serveur est utilisé par défaut)';
