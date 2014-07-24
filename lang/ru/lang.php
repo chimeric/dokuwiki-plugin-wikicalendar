551a5872e0a9372c80dfa33f374a8205e4cb4d41
SQReder <sqreder@gmail.com>
2014-07-24 20:00:34 +0400

    Add russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Russian Language File
+ * @author Popovskiy Andrey <sqreder@gmail.com>
+ */
+
+$lang['current']    = 'Сегодня';
+$lang['go']         = 'Перейти';
+
+$lang['year']       = 'Год';
+$lang['mon']        = 'Месяц';
+
+$lang['days'][1]    = 'Понедельник';
+$lang['days'][2]    = 'Вторник';
+$lang['days'][3]    = 'Среда';
+$lang['days'][4]    = 'Четверг';
+$lang['days'][5]    = 'Пятница';
+$lang['days'][6]    = 'Суббота';
+$lang['days'][7]    = 'Воскресение';
+
+$lang['month'][1]   = 'Январь';
+$lang['month'][2]   = 'Февраль';
+$lang['month'][3]   = 'Март';
+$lang['month'][4]   = 'Апрель';
+$lang['month'][5]   = 'Май';
+$lang['month'][6]   = 'Июнь';
+$lang['month'][7]   = 'Июль';
+$lang['month'][8]   = 'Август';
+$lang['month'][9]   = 'Сентябрь';
+$lang['month'][10]  = 'Октябрь';
+$lang['month'][11]  = 'Ноябрь';
+$lang['month'][12]  = 'Декабрь';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ lang/ru/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Russian settings file for DokuWiki Plugin wikicalendar
+ * @author Popovskiy Andrey <sqreder@gmail.com>
+ */
+$lang['weekstart'] = 'Первый день недели';
+$lang['timezone']  = 'Временная зоня (по умолчанию используется зона сервера)';
